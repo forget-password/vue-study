bd20b15b6cb5ca25c5ca4361c85b485819d7f4b0
maolin <maolin.yin@tarsocial.com>
2024-11-28 18:23:02 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about" v-bind:title="data">
+    <h1 v-on:click="handle">This is an about page {{ data }} --</h1>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    data: 22,
+  }),
+  methods: {
+    handle(e) {
+      console.log(e, "=====", 22);
+    },
+  },
+};
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
